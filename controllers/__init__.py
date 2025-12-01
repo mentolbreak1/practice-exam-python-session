5fefc97a5545ba6f4efd9f8d0bcb9cfa4780bb8f
mentolbreak1 <98565793+mentolbreak1@users.noreply.github.com>
2025-12-01 14:05:49 +0300

    Update __init__.py

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,2 +1,6 @@
 # Пакет контроллеров 
+from .task_controller import TaskController
+from .project_controller import ProjectController
+from .user_controller import UserController
 
+__all__ = ['TaskController', 'ProjectController', 'UserController']
